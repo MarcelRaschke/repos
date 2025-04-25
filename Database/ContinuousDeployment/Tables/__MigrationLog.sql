5fed00918befceda801d05b2db7e835576be89fd
Michi <Michi@ACER>
2025-04-25 08:45:49 +0200

    Merge branch 'main' of https://github.com/MarcelRaschke/repos
    Neue Visual Studio-Lösung und Layout-Updates
    
    Hinzufügung einer neuen Visual Studio-Lösung (`downloader2.sln`) mit spezifischen Debugging- und Projekteinstellungen.
    Aktualisierung der `.suo`-Dateien zur Speicherung des Benutzeroberflächenzustands und der zuletzt geöffneten Projekte.
    Die `DocumentLayout.backup.json` und `DocumentLayout.json` wurden aktualisiert, um neue Dokumentgruppen und Lesezeichen hinzuzufügen, was die Organisation und den Zugriff auf Dokumente verbessert.
    
    Add initial database project structure and migration logging
    
    - Created __MigrationLog.sql for tracking migration details.
    - Added Database.sln solution file for Visual Studio integration.
    - Created Database.sqlproj for managing database project settings and configurations.
    - Implemented pre-deployment and post-deployment scripts for handling deployment processes.
    - Included necessary folders and build references in the project file.
    - Added assembly reference cache for debugging purposes.

---
+++ Database/ContinuousDeployment/Tables/__MigrationLog.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[__MigrationLog]
+(
+	[SqlHash] BINARY(64) NULL, 
+    [StartTime] DATETIME2 NULL, 
+    [EndTime] DATETIME2 NULL, 
+    [SqlText] NVARCHAR(MAX) NULL,
+    [WasSuccessful] BIT NULL DEFAULT 0, 
+    [Error] NVARCHAR(MAX) NULL,
+    [Author] NVARCHAR(250) NULL, 
+    [Build] NVARCHAR(50) NULL,
+	CONSTRAINT [PK_MigrationLog] PRIMARY KEY CLUSTERED ([SqlHash] ASC),
+)
